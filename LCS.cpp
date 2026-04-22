e200af207bb659d436cb3ed90771da34a2dd2bc9
parneetkaur-2006 <kaurparneet0083@gmail.com>
2026-04-22 21:32:32 +0530

    Create LCS.cpp

---
+++ LCS.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>   
+using namespace std;
+
+int main() {
+    string s1, s2;
+
+    cout << "Enter two strings:\n";
+    cin >> s1 >> s2;
+
+    int n = s1.size();
+    int m = s2.size();
+
+    // DP table
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    // Build DP table
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+
+            if(s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    int lcsLength = dp[n][m];
+
+    // Backtracking to find sequence
+    string lcs = "";
+    int i = n, j = m;
+
+    while(i > 0 && j > 0) {
+        if(s1[i - 1] == s2[j - 1]) {
+            lcs += s1[i - 1];
+            i--;
+            j--;
+        }
+        else if(dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        }
+        else {
+            j--;
+        }
+    }
+
+    // reverse LCS
+    reverse(lcs.begin(), lcs.end());
+
+    cout << "\nLCS Length: " << lcsLength << endl;
+    cout << "LCS Sequence: " << lcs << endl;
+
+    return 0;
+}
